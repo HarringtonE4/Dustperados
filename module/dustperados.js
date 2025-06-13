e9eef0f24e81c4f8b6c4b32a6ae1a05ec64dd153
HarringtonE4 <100445878+HarringtonE4@users.noreply.github.com>
2025-06-12 20:32:29 -0700

    Added Luck/Jinx

--- module/dustperados.js
+++ module/dustperados.js
@@ -10,6 +10,50 @@ import { DustperadosItemSheet } from "./sheets/item-sheet.js";
 Hooks.once("init", async function() {
     console.log("Dustperados | Initializing Dustperados System (Init Hook)");
 
+    // --- Define Custom Status Effects ---
+    // This array will replace the default Foundry status effects.
+    const DUSTPERADOS_STATUS_EFFECTS = [
+        {
+            id: 'dying',
+            label: 'DUSTPERADOS.Condition.Dying',
+            icon: 'icons/svg/skull.svg'
+        },
+        {
+            id: 'unconscious',
+            label: 'DUSTPERADOS.Condition.Unconscious',
+            icon: 'icons/svg/stoned.svg'
+        },
+        {
+            id: 'halfCover',
+            label: 'DUSTPERADOS.Condition.HalfCover',
+            icon: 'icons/svg/barrel.svg' // Using aura to suggest partial protection
+        },
+        {
+            id: 'fullCover',
+            label: 'DUSTPERADOS.Condition.FullCover',
+            icon: 'icons/svg/shield.svg' // Using a solid shield for full protection
+        },
+        {
+            id: 'kneeling',
+            label: 'DUSTPERADOS.Condition.Kneeling',
+            icon: 'icons/svg/direction.svg'
+        },
+        {
+            id: 'prone',
+            label: 'DUSTPERADOS.Condition.Prone',
+            icon: 'icons/svg/falling.svg'
+        },
+        {
+            id: 'riding',
+            label: 'DUSTPERADOS.Condition.Riding',
+            icon: 'icons/svg/angel.svg' // Placeholder, suggests "mounted up"
+        },
+    ];
+
+    // Overwrite the default status effects with our custom list.
+    CONFIG.statusEffects = DUSTPERADOS_STATUS_EFFECTS;
+
+
     const templatePaths = []; 
     await foundry.applications.handlebars.loadTemplates(templatePaths); 
 });
