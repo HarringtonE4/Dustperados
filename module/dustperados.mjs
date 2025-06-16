4618fc0c1054ab7f447259a9d487b2ac3ed1ee62
HarringtonE4 <100445878+HarringtonE4@users.noreply.github.com>
2025-06-16 13:47:19 -0700

    Dunno

--- module/dustperados.mjs
+++ module/dustperados.mjs
@@ -1,17 +1,7 @@
-// Dustperados/module/dustperados.js
-
 import { DustperadosActorSheet } from "./sheets/actor-sheet.js";
 import { DustperadosItemSheet } from "./sheets/item-sheet.js";
 import { DUSTPERADOS } from "./config.js";
 
-// Add this right below your import statements
-const DEFAULT_CHARACTER_DATA = {
-    "description": { "type": "String", "label": "Description", "value": "" },
-    "abilities": { "brawn": { "value": 0 }, "quickness": { "value": 0 }, "resolve": { "value": 0 }, "charm": { "value": 0 }, "wits": { "value": 0 } },
-    "hitLocations": { "head": { "value": 0, "max": 0 }, "body": { "value": 0, "max": 0 }, "leftArm": { "value": 0, "max": 0 }, "rightArm": { "value": 0, "max": 0 }, "leftLeg": { "value": 0, "max": 0 }, "rightLeg": { "value": 0, "max": 0 }, "defense": { "value": 0 } },
-    "damageStatus": { "graze": { "value": 0 }, "hit": { "value": 0 }, "crit": { "value": 0 } },
-    "condition": { "luckjinx": { "value": 0, "max": 5, "min": -5 } }
-};
 
 class DustperadosSystem {
     constructor() {
@@ -78,34 +68,3 @@ class DustperadosSystem {
         });
     }
 }
-
-// Register hooks to call the class methods
-Hooks.once("init", () => {
-    // Instantiate the system class inside the init hook
-    game.dustperados = new DustperadosSystem();
-    // Call the init method
-    game.dustperados.onInit();
-});
-
-Hooks.once("ready", () => {
-    // By the time the ready hook fires, game.dustperados is guaranteed to exist.
-    game.dustperados.onReady();
-});
-
-// Replace your old preCreateActor hook with this one
-Hooks.on("preCreateActor", (actor, data, options, userId) => {
-    // --- DIAGNOSTIC TEST ---
-    console.log("Dustperados | Firing preCreateActor hook.");
-
-    // If the system data is missing when we try to create a character...
-    if ( data.type === "character" && !data.system ) {
-        console.log("Dustperados | --- System data is MISSING. Forcing default data. ---");
-
-        // ...manually insert our default data model.
-        // We use deepClone to make sure it's a fresh copy.
-        actor.updateSource({
-            "system": foundry.utils.deepClone(DEFAULT_CHARACTER_DATA)
-        });
-        console.log("Dustperados | --- Actor data has been manually injected. ---");
-    }
-});
